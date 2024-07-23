87bd18e5bccab5b75a45bfdcba43a6e10372c3a1
Jinsik <ladiescheong@naver.com>
2024-07-23 10:20:09 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s[] = {"Total days in Year", "365", "Circumference rate", "3.1415926535"};
+    for (string str : s) cout << str << endl;
+    return 0;
+}
