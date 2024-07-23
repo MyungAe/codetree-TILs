33dd5e9805543f5a6943f1f1577f0852ec099d05
Jinsik <ladiescheong@naver.com>
2024-07-23 10:21:50 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2;
+    char c = 'C';
+    string s = "->";
+
+    cout << a << s << b << s << c;
+    return 0;
+}
