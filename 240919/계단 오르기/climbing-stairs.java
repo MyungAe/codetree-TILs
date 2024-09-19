ead0d932250d150039b03eb9f65f1c2332109abe
Jinsik <ladiescheong@naver.com>
2024-09-19 16:47:14 +0900

    Title: 계단 오르기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240919/계단 오르기/climbing-stairs.java
+++ 240919/계단 오르기/climbing-stairs.java
@@ -13,11 +13,22 @@ public class Main {
         System.out.println(step(N));
     }
 
-    public static int step(int N) {
-        if (dp[N] != -1) return dp[N];
-        if (N == 0 || N == 2 || N == 3) return dp[N] = 1;
-        if (N == 1) return dp[N] = 0;
+    // public static int step(int N) {
+    //     if (dp[N] != -1) return dp[N];
+    //     if (N == 0 || N == 2 || N == 3) return dp[N] = 1;
+    //     if (N == 1) return dp[N] = 0;
 
-        return dp[N] = (step(N - 2) + step(N - 3)) % 10007;
+    //     return dp[N] = (step(N - 2) + step(N - 3)) % 10007;
+    // }
+
+    public static int step(int N) {
+        dp[0] = dp[2] = dp[3] = 1;
+        dp[1] = 0;
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
+        }
+
+        return dp[N];
     }
 }
