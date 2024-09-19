80f6922e1f3f5864432fffd649591febb277975d
Jinsik <ladiescheong@naver.com>
2024-09-19 20:03:49 +0900

    Title: 사각형 채우기 2, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

--- 240919/사각형 채우기 2/rectangle-fill-2.java
+++ 240919/사각형 채우기 2/rectangle-fill-2.java
@@ -10,7 +10,7 @@ public class Main {
         dp[1] = 1; dp[2] = 3;
 
         for (int i = 3; i <= N; i++) {
-            dp[i] = (dp[i - 1] * dp[i - 2] + (dp[i - 1] - 1) * dp[i - 2]) % 10007;
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
         }
 
         System.out.println(dp[N]);
