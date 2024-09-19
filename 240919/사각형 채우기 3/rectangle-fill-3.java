6b93b3ab1237a0b28e9f8944e898c1140c176f0b
Jinsik <ladiescheong@naver.com>
2024-09-19 17:32:24 +0900

    Title: 사각형 채우기 3, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240919/사각형 채우기 3/rectangle-fill-3.java
+++ 240919/사각형 채우기 3/rectangle-fill-3.java
@@ -2,23 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int[] dp = new int[1001];
+    static long[] dp = new long[1001];
+    static final int mod = 1000000007;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Arrays.fill(dp, -1);
-
         System.out.println(step(N));
     }
 
-    public static int step(int N) {
+    public static long step(int N) {
         dp[0] = 1; dp[1] = 2;
         
         for (int i = 2; i <= N; i++) {
-            dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % 1000000007;
+            dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2]) % mod;
             for (int j = i - 3; j >= 0; j--) {
-                dp[i] = (dp[i] + 2 * dp[j]) % 1000000007;
+                dp[i] = (dp[i] + 2 * dp[j]) % mod;
             }
         }
 
