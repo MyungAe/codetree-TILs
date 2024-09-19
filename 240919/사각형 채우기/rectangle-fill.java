50a288f8c22195a95c03bcbe1ea8b2423e50c5e6
Jinsik <ladiescheong@naver.com>
2024-09-19 16:53:14 +0900

    Title: 사각형 채우기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240919/사각형 채우기/rectangle-fill.java
+++ 240919/사각형 채우기/rectangle-fill.java
@@ -13,11 +13,22 @@ public class Main {
         System.out.println(step(N));
     }
 
-    public static int step(int N) {
-        if (dp[N] != -1) return dp[N];
-        if (N == 1) return dp[N] = 1;
-        if (N == 2) return dp[N] = 2;
+    // public static int step(int N) {
+    //     if (dp[N] != -1) return dp[N];
+    //     if (N == 1) return dp[N] = 1;
+    //     if (N == 2) return dp[N] = 2;
 
-        return dp[N] = (step(N - 1) + step(N - 2)) % 10007;
+    //     return dp[N] = (step(N - 1) + step(N - 2)) % 10007;
+    // }
+
+    public static int step(int N) {
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[N];
     }
 }
