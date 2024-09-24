f1f33f215b5c398064487aee32ac1849698802b0
Jinsik <ladiescheong@naver.com>
2024-09-24 18:48:28 +0900

    Title: 동전 거슬러주기, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240924/동전 거슬러주기/coin-change.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] coin, dp;
+    static StringTokenizer st;
+    static BufferedReader br;
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        for (int i = 1; i <= M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i >= coin[j]) {
+                    if (dp[i - coin[j]] == Integer.MAX_VALUE) continue;
+                    dp[i] = Math.min(dp[i], dp[i - coin[j]] + 1);
+                }
+            }
+        }
+
+        // System.out.println(Arrays.toString(dp));
+
+        int ans = dp[M];
+        if (ans == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(ans);
+    }
+
+    public static void init() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        coin = new int[] {0, 1, 4, 5};
+        dp = new int[M + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+    }
+}
