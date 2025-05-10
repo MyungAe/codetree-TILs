de035ae4811cacd5e740dfdfaf5e9621f9e1ca32
Jinsik <ladiescheong@naver.com>
2025-05-10 20:10:04 +0900

    Title: 0 나올때까지 출력, Time: 189ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/0 나올때까지 출력/print-until-zero-is-given.js
+++ 250510/0 나올때까지 출력/print-until-zero-is-given.js
@@ -1,3 +1,3 @@
 const arr = require('fs').readFileSync(0).toString().trim().split("\n").map(e => Number(e));
 
-arr.forEach(num => num === 0 ? console.log(num) : '');
+arr.forEach(num => num !== 0 ? console.log(num) : '');
