ce6cef6129478c9551366573ba630b568984b219
Jinsik <ladiescheong@naver.com>
2025-05-10 18:00:25 +0900

    Title: 입력받은 값과 합 출력, Time: 179ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250510/입력받은 값과 합 출력/output-of-value-and-sum.js
+++ 250510/입력받은 값과 합 출력/output-of-value-and-sum.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/sit').toString().trim().split(' ');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
 console.log(`${input[0]} ${input[1]} ${input[0] + input[1]}`);
