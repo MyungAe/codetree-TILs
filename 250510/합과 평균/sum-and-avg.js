869bd5ce0609170d4bb60a686e83ccf61834cbc0
Jinsik <ladiescheong@naver.com>
2025-05-10 18:03:34 +0900

    Title: 합과 평균, Time: 185ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/합과 평균/sum-and-avg.js
+++ 250510/합과 평균/sum-and-avg.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
-console.log(`${Number(input[0]) + Number(input[1])} ${(Number(input[0]) + Number(input[1])) / 2}`)
+console.log(`${Number(input[0]) + Number(input[1])} ${((Number(input[0]) + Number(input[1])) / 2).toFixed(1)}`)
