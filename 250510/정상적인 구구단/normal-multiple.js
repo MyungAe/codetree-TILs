017261c9b20a2fa1e487c0131434a5d64b3471d5
Jinsik <ladiescheong@naver.com>
2025-05-10 20:26:45 +0900

    Title: 정상적인 구구단, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/정상적인 구구단/normal-multiple.js
+++ 250510/정상적인 구구단/normal-multiple.js
@@ -3,7 +3,8 @@ const N = Number(require('fs').readFileSync(0).toString().trim());
 let answer = '';
 for(let i = 1; i <= N; i++) {
     for (let j = 1; j <= N; j++) {
-        answer += `${i} * ${j} = ${i * j}, `
+        answer += `${i} * ${j} = ${i * j}`; 
+        j !== N ? answer += ', ' : null;
     }
     answer += '\n';
 }
