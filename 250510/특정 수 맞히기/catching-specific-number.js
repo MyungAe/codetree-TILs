3daa74da650ca4160fb26bfe544db0e78593d923
Jinsik <ladiescheong@naver.com>
2025-05-10 20:15:49 +0900

    Title: 특정 수 맞히기, Time: 188ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/특정 수 맞히기/catching-specific-number.js
+++ 250510/특정 수 맞히기/catching-specific-number.js
@@ -2,8 +2,8 @@ const arr = require('fs').readFileSync(0).toString().trim().split("\n").map(e =>
 
 for (let i = 0; i < arr.length; i++) {
     console.log(
-        arr[i] > 25 ? 'Higher' :
-        arr[i] < 25 ? 'Lower' : "Good"
+        arr[i] > 25 ? 'Lower' :
+        arr[i] < 25 ? 'Higher' : "Good"
     )
     if (arr[i] === 25) break;
 }
