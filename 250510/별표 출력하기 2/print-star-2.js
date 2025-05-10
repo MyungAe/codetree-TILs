8272f168bc7adf8aa41818825c45e798d42518b7
Jinsik <ladiescheong@naver.com>
2025-05-10 20:20:39 +0900

    Title: 별표 출력하기 2, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/별표 출력하기 2/print-star-2.js
+++ 250510/별표 출력하기 2/print-star-2.js
@@ -3,8 +3,9 @@ const N = Number(require('fs').readFileSync(0).toString().trim());
 let answer = '';
 for (let i = 0; i < N; i++) {
     for (let j = i; j < N; j++) {
-        answer += "* "
+        answer += "* ";
     }
+    answer += '\n'
 }
 
 console.log(answer);
