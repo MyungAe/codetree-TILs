2914d9bcc3193c76ff844fe0b40ebfebc4c298c1
Jinsik <ladiescheong@naver.com>
2025-05-10 19:02:32 +0900

    Title: B부터 A까지 감소, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

--- 250510/B부터 A까지 감소/dec-from-b-to-a.js
+++ 250510/B부터 A까지 감소/dec-from-b-to-a.js
@@ -1,4 +1,4 @@
-const [A, B] = require('fs').toString().trim().split(' ').map(e => Number(e));
+const [A, B] = require('fs').readFileSync(0).toString().trim().split(' ').map(e => Number(e));
 
 let answer = '';
 for (let i = B; i >= A; i--) { 
