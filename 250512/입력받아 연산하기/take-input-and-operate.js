7ec14f7a5100e025640022ac2f6fff3590f8af57
Jinsik <ladiescheong@naver.com>
2025-05-12 22:44:35 +0900

    Title: 입력받아 연산하기, Time: 179ms, Memory: 15MB, Status: Passed - Codetree

--- 250512/입력받아 연산하기/take-input-and-operate.js
+++ 250512/입력받아 연산하기/take-input-and-operate.js
@@ -1,4 +1,4 @@
-const [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(e => Number(e));
+const [a, b] = require('fs').readFileSync(0).toString().trim().split('\n').map(e => Number(e));
 
 console.log(a + 87);
 console.log(b % 10);
