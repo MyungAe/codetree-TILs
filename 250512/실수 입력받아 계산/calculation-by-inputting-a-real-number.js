b22a521acfa15333b9266dde599b016bca7d6aad
Jinsik <ladiescheong@naver.com>
2025-05-12 22:20:06 +0900

    Title: 실수 입력받아 계산, Time: 200ms, Memory: 15MB, Status: Passed - Codetree

--- 250512/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
+++ 250512/실수 입력받아 계산/calculation-by-inputting-a-real-number.js
@@ -1,3 +1,3 @@
-const [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(e => Number(e));
+const [a, b] = require('fs').readFileSync(0).toString().trim().split('\n').map(e => Number(e));
 
 console.log((a + b).toFixed(2))
