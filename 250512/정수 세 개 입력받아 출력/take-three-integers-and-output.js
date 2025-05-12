7037d5fc564c6987d72d1dc029b421dd5bda69e6
Jinsik <ladiescheong@naver.com>
2025-05-12 22:23:08 +0900

    Title: 정수 세 개 입력받아 출력, Time: 187ms, Memory: 15MB, Status: Passed - Codetree

--- 250512/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 250512/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,3 +1,3 @@
-const [[a, b], c] = require('fs').readFileSync(0).toString().trim().split('\n').map(e => e.split(' ').map(e => Number(e)));
+const [[a, b], [c]] = require('fs').readFileSync(0).toString().trim().split('\n').map(e => e.split(' ').map(e => Number(e)));
 
 console.log(a, b, c);
