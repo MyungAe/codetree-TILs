4b45ff232df1ef1b7f114883c1b48dafe78889f0
Jinsik <ladiescheong@naver.com>
2025-07-03 22:02:49 +0900

    Title: 방향에 맞춰 이동, Time: 183ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250703/방향에 맞춰 이동/move-in-direction.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const moves = input.slice(1);
+
+// Please Write your code here.
+const dx = [0, 0, 1, -1];
+const dy = [1, -1, 0, 0];
+
+let location = [0, 0];
+const directionMap = {
+    'N' : 0,
+    'E' : 2,
+    'S' : 1,
+    'W' : 3
+};
+
+moves.forEach(([direction, value]) => {
+    location[0] += dx[directionMap[direction]] * value;
+    location[1] += dy[directionMap[direction]] * value;
+})
+
+console.log(location.join(" "));
